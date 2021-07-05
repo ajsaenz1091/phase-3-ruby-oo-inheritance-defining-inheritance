931ffe86bd3cb622e713e4df057ee145b01f34a9
Alberto <ajsaenz1091@gmail.com>
2021-07-05 19:33:17 -0400

    commit tree f03dee34bbecf3193e836c19a11f77ef868800aa

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,20 @@
 require_relative "./vehicle.rb"
+
+class Car < Vehicle
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+    # attr_accessor :wheel_size , :number
+
+    # @@all = []
+
+    # def initialize(wheel_size, number)
+    #     @wheel_size = wheel_size
+    #     @numeber = number
+    #     @@all << self
+    # end
+
+    # def self.all
+    #     @@all
+    # end
+end
